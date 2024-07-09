440eda849ebed58f93468bbf2dd7602c126b8e1f
Michael Simons <msimons@microsoft.com>
2024-07-09 10:05:54 -0500

    Edits

--- eng/tools/tasks/Microsoft.DotNet.UnifiedBuild.Tasks/ScanSbrps.cs
+++ eng/tools/tasks/Microsoft.DotNet.UnifiedBuild.Tasks/ScanSbrps.cs
@@ -2,11 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Xml;
 using System.Xml.Linq;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
@@ -28,30 +26,25 @@ namespace Microsoft.DotNet.UnifiedBuild.Tasks
         [Required]
         public string SbrpRepoPath { get; set; }
 
-        private Dictionary<string, PackageInfo> _sbrpPackages = new();
+        private readonly Dictionary<string, PackageInfo> _sbrpPackages = [];
 
         public override bool Execute()
         {
-            IList<string> filesWithoutPDBs = GenerateSymbolsLayout(IndexAllSymbols());
-            if (filesWithoutPDBs.Count > 0)
-            {
-                LogErrorOrWarning(FailOnMissingPDBs, $"Did not find PDBs for the following SDK files:");
-                foreach (string file in filesWithoutPDBs)
-                {
-                    LogErrorOrWarning(FailOnMissingPDBs, file);
-                }
-           }
+            ReadSbrpPackages("referencePackages");
+            ReadSbrpPackages("textOnlyPackages");
 
             return !Log.HasLoggedErrors;
         }
 
-        private void ReadSbrpPackages()
+        private string GetSBRPPackagesPath(string packageType) => Path.Combine(SbrpRepoPath, "src", packageType, "src");
+
+        private void ReadSbrpPackages(string packageType)
         {
-            EnumerationOptions options = new() { RecurseSubdirectories = true};
-            
-            foreach (string projectFile in Directory.GetFiles(SbrpRepoPath, "*.csproj", options))
+            EnumerationOptions options = new() { RecurseSubdirectories = true };
+
+            foreach (string projectPath in Directory.GetFiles(GetSBRPPackagesPath(packageType), "*.csproj", options))
             {
-                XDocument xmlDoc = XDocument.Load(projectFile);
+                XDocument xmlDoc = XDocument.Load(projectPath);
                 IEnumerable<string> tfms = xmlDoc.Element("Project")?
                     .Elements("PropertyGroup")
                     .Elements("TargetFrameworks")
@@ -59,30 +52,33 @@ namespace Microsoft.DotNet.UnifiedBuild.Tasks
 
                 if (tfms == null || !tfms.Any())
                 {
-                     Log.LogError($"No TargetFrameworks were delected in {projectFile}.");
+                    Log.LogError($"No TargetFrameworks were delected in {projectPath}.");
                 }
 
-                string version = Directory.GetParent(projectFile).Name;
-                string projectName = Path.GetFileNameWithoutExtension(projectFile);
-                PackageInfo info = new ()
+                string version = Directory.GetParent(projectPath).Name;
+                string projectName = Path.GetFileNameWithoutExtension(projectPath);
+                PackageInfo info = new()
                 {
                     Version = version,
-                    Name = projectName.Substring(0, projectName.Length - 1 - version.Length),
-                    TFMs = new HashSet<string>(tfms),
+                    Name = projectName[..(projectName.Length - 1 - version.Length)],
+                    Type = packageType,
+                    Tfms = new HashSet<string>(tfms),
                 };
 
-                sbrps.Add($"{info.Id}", info);
+                _sbrpPackages.Add($"{info.Id}", info);
+                Log.LogMessage($"Detected SBRP Package: {info.Id} {packageType}");
             }
         }
 
         private class PackageInfo
         {
             public string Id => $"{Name}/{Version}";
-            public string Name {get; set;}
-            public string Version {get; set;}
-            public HashSet<string> Tfms {get; set; }
-            public HashSet<string> References { get; } = new ();
-            public HashSet<string> ReferencedTfms { get; } = new ();
+            public string Name { get; set; }
+            public string Version { get; set; }
+            public string Type{ get; set; }
+            public HashSet<string> Tfms { get; set; }
+            public HashSet<string> References { get; } = [];
+            public HashSet<string> ReferencedTfms { get; } = [];
         }
     }
 }
